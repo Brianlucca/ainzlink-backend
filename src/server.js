8dbcb17df313b4cfb4c69ca3ec1b57241d0b9e51
Brian Lucca <brianlucca2002@gmail.com>
2025-08-20 19:43:07 -0300

    fix: enable and configure cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import './config/firebase.js';
 
@@ -8,12 +9,12 @@ import redirectRoutes from './routes/redirectRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1', urlRoutes);
-
 app.use('/', redirectRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor escutando na porta ${PORT}`);
 });
