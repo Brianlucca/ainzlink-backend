64650f760fcb441aabbe2fe56508e9001c40db3c
Brian Lucca <brianlucca2002@gmail.com>
2025-08-20 16:44:09 -0300

    feat: initialize express server and connect to firebase

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
+import './config/firebase.js';
+import urlRoutes from './routes/urlRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -9,6 +11,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.status(200).send({ message: 'API do AinzLink está no ar!' });
 });
+
+app.use('/api/v1', urlRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta http://localhost:${PORT}`);
