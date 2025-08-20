1bc1d19a831f42cbba2bc61851eb8b35f410853c
Brian Lucca <brianlucca2002@gmail.com>
2025-08-20 19:48:39 -0300

    feat: add health check route at root path.

--- src/server.js
+++ src/server.js
@@ -12,8 +12,14 @@ const PORT = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'AinzLink API está no ar e operacional!' });
+});
+
+
 app.use('/api/v1', urlRoutes);
 app.use('/', redirectRoutes);
+
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor escutando na porta ${PORT}`);
