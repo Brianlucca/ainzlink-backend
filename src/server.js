1792693b03f13e9bebd1922120f99153888b2ebc
Brian Lucca <brianlucca2002@gmail.com>
2025-08-20 21:26:38 -0300

    fix: add route handler for favicon.ico to prevent ghost requests

--- src/server.js
+++ src/server.js
@@ -1,26 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import http from 'http';
-import { Server } from 'socket.io';
-
 import './config/firebase.js';
-import initializeSocket from './websockets.js';
-
 import urlRoutes from './routes/urlRoutes.js';
 import redirectRoutes from './routes/redirectRoutes.js';
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
-
-initializeSocket(io);
-
 const PORT = process.env.PORT || 5001;
 
 app.use(cors());
@@ -30,9 +15,15 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'AinzLink API está no ar e operacional!' });
 });
 
+
+// Isso impede que a requisição automática do navegador por um ícone seja contada como um clique.
+app.get('/favicon.ico', (req, res) => res.status(204).send());
+
+
 app.use('/api/v1/urls', urlRoutes);
 app.use('/', redirectRoutes);
 
-server.listen(PORT, '0.0.0.0', () => {
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor escutando na porta ${PORT}`);
 });
